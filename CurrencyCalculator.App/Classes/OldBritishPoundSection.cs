a620d28ce286646ae33a6b3cb26c39435d05d5e3
Francesco Sciuto <sciuto.francesco@gmail.com>
2021-11-07 20:36:00 +0100

    Add class to store OldBritishPound sections' values

---
+++ CurrencyCalculator.App/Classes/OldBritishPoundSection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyCalculator.App.Classes
+{
+    public class OldBritishPoundSection
+    {
+        public int Value { get; set; }
+        public int Remainder { get; set; }
+
+        public OldBritishPoundSection()
+        {
+            Value = 0;
+            Remainder = 0;
+        }
+    }
+}
