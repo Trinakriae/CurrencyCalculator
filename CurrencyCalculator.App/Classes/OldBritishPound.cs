85ac987ab934b0ced37be113f9b0e7b339dcb7ca
Francesco Sciuto <sciuto.francesco@gmail.com>
2021-11-07 20:39:47 +0100

    Add initialization for OldBritishPound

--- CurrencyCalculator.App/Classes/OldBritishPound.cs
+++ CurrencyCalculator.App/Classes/OldBritishPound.cs
@@ -8,10 +8,23 @@ namespace CurrencyCalculator.App.Classes
 
     public class OldBritishPound : ICurrency
     {
+        public const int SHILLING_TO_POUND = 20;
+        public const int PENNY_TO_SHILLING = 12;
+
+        public OldBritishPoundSection Pound { get; set; }
+        public OldBritishPoundSection Shilling { get; set; }
+        public OldBritishPoundSection Penny { get; set; }
+
         public OldBritishPound(string value)
         {
 
+            var subStrings = value.Split(' ');
+
+            this.Pound.Value = Decode(subStrings[0]);
+            this.Shilling.Value = Decode(subStrings[1]);
+            this.Penny.Value = Decode(subStrings[2]);
         }
+
 
         public ICurrency Add(string adder)
         {
@@ -32,5 +45,12 @@ namespace CurrencyCalculator.App.Classes
         {
             throw new NotImplementedException();
         }
+
+        private int Decode(string toDecode)
+        {
+            string sanitizedValue = toDecode.Remove(toDecode.Length - 1, 1);
+            return Int32.Parse(sanitizedValue);
+        }
+
     }
 }
