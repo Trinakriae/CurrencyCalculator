522612b49c860dd014e66d616455aee276653e1b
Francesco Sciuto <sciuto.francesco@gmail.com>
2021-11-07 20:31:08 +0100

    Add OldBritishPound class implementing ICurrency

---
+++ CurrencyCalculator.App/Classes/OldBritishPound.cs
@@ -0,0 +1,36 @@
+﻿using CurrencyCalculator.App.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyCalculator.App.Classes
+{
+
+    public class OldBritishPound : ICurrency
+    {
+        public OldBritishPound(string value)
+        {
+
+        }
+
+        public ICurrency Add(string adder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICurrency Divide(int divider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICurrency Multiply(int multiplier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICurrency Subtract(string subtractor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
