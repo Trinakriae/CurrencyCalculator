d8f20d790251210f5bf96f2889d5b1e101233263
Francesco Sciuto <sciuto.francesco@gmail.com>
2021-11-07 20:23:23 +0100

    Remove double namespace

--- CurrencyCalculator.App/Interfaces/ICurrency.cs
+++ CurrencyCalculator.App/Interfaces/ICurrency.cs
@@ -1,15 +1,11 @@
 ﻿namespace CurrencyCalculator.App.Interfaces
 {
-    namespace CurrencyCalculator.Interfaces
+    public interface ICurrency
     {
-        public interface ICurrency
-        {
-            ICurrency Add(string adder);
-            ICurrency Subtract(string subtractor);
-            ICurrency Multiply(int multiplier);
-            ICurrency Divide(int divider);
-            string ToString();
-        }
+        ICurrency Add(string adder);
+        ICurrency Subtract(string subtractor);
+        ICurrency Multiply(int multiplier);
+        ICurrency Divide(int divider);
+        string ToString();
     }
-
 }
