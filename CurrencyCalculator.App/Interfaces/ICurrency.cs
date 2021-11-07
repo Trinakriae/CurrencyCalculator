0faeb6377dcd7a81ae1e9a463fffbf8668a709d6
Francesco Sciuto <sciuto.francesco@gmail.com>
2021-11-07 20:17:00 +0100

    Add ICurrency interface

---
+++ CurrencyCalculator.App/Interfaces/ICurrency.cs
@@ -0,0 +1,15 @@
+﻿namespace CurrencyCalculator.App.Interfaces
+{
+    namespace CurrencyCalculator.Interfaces
+    {
+        public interface ICurrency
+        {
+            ICurrency Add(string adder);
+            ICurrency Subtract(string subtractor);
+            ICurrency Multiply(int multiplier);
+            ICurrency Divide(int divider);
+            string ToString();
+        }
+    }
+
+}
